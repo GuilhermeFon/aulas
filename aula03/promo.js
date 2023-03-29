ec870a7df7e790cea5453c408cce6decac06d330
GuilhermeFon <guilhermelimafonseca@gmail.com>
2023-03-29 19:45:19 -0300

    feat: add tofixed in total price

--- aula03/promo.js
+++ aula03/promo.js
@@ -6,4 +6,4 @@ const price = Number(prompt("Valor do seu produto: "));
 const total = Math.floor(price) * 2;
 
 console.log(`Promoção de ${product}`);
-console.log(`Na compra de 2 unidades, o total é: ${total}`);
+console.log(`Na compra de 2 unidades, o total é: ${total.toFixed(2)}`);
